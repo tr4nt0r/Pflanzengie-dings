706188e831ff5711493dfbb50f45ad3bb370a3ec
tr4nt0r <manni@zapto.de>
2018-05-15 18:26:52 +0200

    Projektdateien hinzufügen.

---
+++ Pflanziegießdings/Pflanziegießdings.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		Pflanziegießdings.ino
+ Created:	15.05.2018 18:26:40
+ Author:	manni
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
