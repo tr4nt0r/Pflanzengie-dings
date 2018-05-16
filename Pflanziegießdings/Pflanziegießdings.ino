abcff8b51e20060a3ba1a8933f85400ef5a79d8d
tr4nt0r <manni@zapto.de>
2018-05-16 14:52:05 +0200

    Initial commit

--- Pflanziegießdings/Pflanziegießdings.ino
+++ Pflanziegießdings/Pflanziegießdings.ino
@@ -4,12 +4,50 @@
  Author:	manni
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+/* Comment this out to disable prints and save space */
+//#define BLYNK_PRINT Serial
 
+
+#include "Credentials.h"
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+
+const uint8_t HumidityAlert = 200;
+const uint32_t HumidityMax = 215;
+const uint32_t HumidityMin = 1010;
+const uint32_t humidityReadInverval = 3e+8;
+
+BlynkTimer timer;
+
+void setup()
+{
+	// Debug console
+	Serial.begin(9600);
+	Blynk.begin(auth, ssid, pass);
+	pinMode(D1, OUTPUT);
+	timer.setInterval(humidityReadInverval, readHumiditySensor);
+	readHumiditySensor();
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() {
-  
+void loop()
+{
+	Blynk.run();
+	timer.run();
+}
+
+
+void readHumiditySensor() {
+	digitalWrite(D1, HIGH);
+	delay(10);
+	uint8_t val = map(analogRead(A0), HumidityMin, HumidityMax, 0UL, 100UL);
+	BLYNK_LOG2("Pushing data...", val);
+	Blynk.virtualWrite(V1, val);
+	digitalWrite(D1, LOW);
+	ESP.deepSleep(humidityReadInverval);
 }
